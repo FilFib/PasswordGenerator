8edc4547666118f9379e5b596b2322aff6408863
Filip Fibakiewicz <fibekb4@gmail.com>
2023-09-30 15:47:10 +0200

    Zakładka Password

--- passwordGenerator/generator/views.py
+++ passwordGenerator/generator/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def password(request):
+    length = request.POST.get('length')
+    big = request.POST.get('big')
+    numbers = request.POST.get('numbers')
+    specials = request.POST.get('specials')
+    return render(request, 'password.html', {'length':length})
