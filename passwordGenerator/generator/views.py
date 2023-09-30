3f182b787170a7efd42131ab2f0a2c70fb4c5313
Filip Fibakiewicz <fibekb4@gmail.com>
2023-09-30 16:00:24 +0200

    Koniec 1 dnia

--- passwordGenerator/generator/views.py
+++ passwordGenerator/generator/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+import random
+import string
 
+def gen_pass(length, capital_letter=False, numbers=False, special_characters=False):
+    characters = string.ascii_lowercase
+
+    if capital_letter:
+        characters += string.ascii_uppercase 
+
+    if numbers:
+        characters += string.digits
+
+    if  special_characters:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    return password
 
 def home(request):
     return render(request, 'home.html')
 
 def password(request):
-    length = request.POST.get('length')
+    length = int(request.POST.get('length'))
     big = request.POST.get('big')
     numbers = request.POST.get('numbers')
     specials = request.POST.get('specials')
-    return render(request, 'password.html', {'length':length})
+    
+    gen_password = gen_pass(length, big, numbers, specials)
+    return render(request, 'password.html', {'password':gen_password})
